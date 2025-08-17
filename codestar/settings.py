fd801af5c579cb2ab5e71b241bc5760d0ad64c77
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-08-17 21:02:26 +0200

    fix: dj-database-url Python 3.13 compatibility

--- codestar/settings.py
+++ codestar/settings.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"), conn_max_age=600, conn_health_checks=True)
 }
 
 
