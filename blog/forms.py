b13991fc41adef6eb881be7a303bce2d8af2737e
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-09-04 20:40:03 +0200

    chore: add learning comments

--- blog/forms.py
+++ blog/forms.py
@@ -2,7 +2,9 @@ from .models import Comment
 from django import forms
 
 
+# Class that will inherit from ModelForms
 class CommentForm(forms.ModelForm):
+    # From the Meta Class select moodels and fields you want
     class Meta:
         model = Comment
         fields = ('body',)
