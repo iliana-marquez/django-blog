585c0aa4dc5bf0c3e5f51fe29bed00d01177e57c
iliana-marquez <y5d7l1us@students.codeinstitute.net>
2025-09-04 20:40:26 +0200

    feat: create CollaborateRequest Model

--- about/models.py
+++ about/models.py
@@ -10,3 +10,14 @@ class About(models.Model):
     # Return a string (the title in this case)
     def __str__(self):
         return self.title
+
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=True)
+
+    # return a string
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
